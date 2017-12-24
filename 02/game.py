c3c3770d67a4a11d7b12c41dd3ad2ebaf2ffc4c2
Samuel Villavicencio <samuelrey010@gmail.com>
2017-12-23 21:36:46 -0800

    Add function stubs.

---
+++ 02/game.py
@@ -0,0 +1,43 @@
+#!python3
+# Code Challenge 02 - Word Values Part II - a simple game
+# http://pybit.es/codechallenge02.html
+
+from data import DICTIONARY, LETTER_SCORES, POUCH
+
+NUM_LETTERS = 7
+
+
+# re-use from challenge 01
+def calc_word_value(word):
+    """Calc a given word value based on Scrabble LETTER_SCORES mapping"""
+    return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
+
+
+# re-use from challenge 01
+def max_word_value(words):
+    """Calc the max value of a collection of words"""
+    return max(words, key=calc_word_value)
+
+
+def draw_letters():
+    pass
+
+
+def get_possible_dict_words():
+    pass
+
+
+def _get_permutations_draw():
+    pass
+
+
+def _validation():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
